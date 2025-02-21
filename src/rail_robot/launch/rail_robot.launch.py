cce1e0e1525d8348d404c74a449a3c76984ff35f
Abhishek Paudel <10024463+abpaudel@users.noreply.github.com>
2025-02-20 19:16:36 -0500

    add robot description

--- src/rail_robot/launch/rail_robot.launch.py
+++ src/rail_robot/launch/rail_robot.launch.py
@@ -2,13 +2,12 @@ import launch
 from launch_ros.actions import Node
 from launch_ros.parameter_descriptions import ParameterFile
 from launch.actions import DeclareLaunchArgument, OpaqueFunction
-from launch.substitutions import LaunchConfiguration
-from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution, Command, FindExecutable
 from launch_ros.substitutions import FindPackageShare
 
 
 def launch_setup(context, *args, **kwargs):
-    robot_name = LaunchConfiguration('robot_name')
+    robot_name_launch_arg = LaunchConfiguration('robot_name')
 
     kobuki_ros_node_parameter_file = ParameterFile(
         param_file=PathJoinSubstitution([
@@ -18,7 +17,6 @@ def launch_setup(context, *args, **kwargs):
         ]),
         allow_substs=True,
     )
-
     slam_toolbox_parameter_file = ParameterFile(
         param_file=PathJoinSubstitution([
             FindPackageShare('rail_robot'),
@@ -27,56 +25,70 @@ def launch_setup(context, *args, **kwargs):
         ]),
         allow_substs=True,
     )
+    robot_description = Command([
+            'sh -c "',
+            FindExecutable(name='xacro'), ' ',
+            PathJoinSubstitution([
+                FindPackageShare('rail_robot'),
+                'urdf',
+                'rail_robot.urdf.xacro'
+            ]), ' ',
+            'robot_name:=', robot_name_launch_arg, ' ',
+            # Stripping comments from the URDF is necessary for gazebo_ros2_control to parse the
+            # robot_description parameter override
+            '| ', FindExecutable(name='perl'), ' -0777 -pe \'s/<!--.*?-->//gs\'"'
+        ])
 
+    robot_state_publisher_node = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        namespace=robot_name_launch_arg,
+        parameters=[{
+            'robot_description': robot_description,
+            'use_sim_time': True,
+        }],
+        output={'both': 'log'},
+    )
     kobuki_ros_node = Node(package='kobuki_node',
                            executable='kobuki_ros_node',
                            name='kobuki_ros_node',
                            output='both',
-                           namespace=robot_name,
+                           namespace=robot_name_launch_arg,
                            parameters=[kobuki_ros_node_parameter_file])
-    lidar_node = Node(package='sllidar_ros2',
+    sllidar_node = Node(package='sllidar_ros2',
                       executable='sllidar_node',
                       name='sllidar_node',
                       output='screen',
-                      namespace=robot_name,
+                      namespace=robot_name_launch_arg,
                       parameters=[{'channel_type': 'serial',
                          'serial_port': '/dev/rplidar',
                          'serial_baudrate': 115200,
-                         'frame_id': (robot_name, '/laser_frame_link'),
+                         'frame_id': (robot_name_launch_arg, '/laser_frame_link'),
                          'inverted': False,
                          'angle_compensate': False}])
-    laser_static_transform_node = Node(package='tf2_ros',
-                                       executable='static_transform_publisher',
-                                       name='static_transform_publisher',
-                                       output='screen',
-                                       arguments=['0', '0', '0', '0', '0', '0',
-                                                  (robot_name, '/base_footprint'),
-                                                  (robot_name, '/laser_frame_link')])
-    mapping_node = Node(package='slam_toolbox',
+    slam_toolbox_node = Node(package='slam_toolbox',
                         executable='async_slam_toolbox_node',
                         name='slam_toolbox',
                         output='screen',
-                        namespace=robot_name,
+                        namespace=robot_name_launch_arg,
                         parameters=[
                             slam_toolbox_parameter_file,
                             {'use_sim_time': True}
                         ],
-                        remappings=[('/map', ('/', robot_name, '/map'))]
+                        remappings=[('/map', ('/', robot_name_launch_arg, '/map'))]
                         )
 
     return [
+        robot_state_publisher_node,
         kobuki_ros_node,
-        lidar_node,
-        laser_static_transform_node,
-        mapping_node
+        sllidar_node,
+        slam_toolbox_node
     ]
 
 
 def generate_launch_description():
     declared_arguments = []
-
-    declared_arguments.append(
-        DeclareLaunchArgument('robot_name', default_value='robot', description='Namespace for the robot')
-    )
+    robot_name_launch_arg = DeclareLaunchArgument('robot_name', default_value='robot', description='Namespace for the robot')
+    declared_arguments.append(robot_name_launch_arg)
     return launch.LaunchDescription(
         declared_arguments + [OpaqueFunction(function=launch_setup)])
