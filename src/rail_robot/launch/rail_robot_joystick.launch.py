b1d852aa5e9fa4f44e7203353f01387154914047
Abhishek Paudel <10024463+abpaudel@users.noreply.github.com>
2025-03-12 20:15:00 -0400

    add joystick launch file (config file not working)

---
+++ src/rail_robot/launch/rail_robot_joystick.launch.py
@@ -0,0 +1,53 @@
+import launch
+from launch_ros.actions import Node
+from launch.actions import DeclareLaunchArgument, OpaqueFunction, GroupAction
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+from launch_ros.substitutions import FindPackageShare
+
+
+def launch_setup(context, *args, **kwargs):
+    robot_name_launch_arg = LaunchConfiguration('robot_name')
+    params_file_launch_arg = LaunchConfiguration('params_file')
+
+    load_nodes = GroupAction(
+        actions=[
+            Node(
+                package='joy', executable='joy_node', name='joy_node',
+                parameters=[{
+                    'device_id': 0,
+                    'deadzone': 0.3,
+                    'autorepeat_rate': 20.0}]
+            ),
+            Node(
+                package='teleop_twist_joy', executable='teleop_node',
+                name='teleop_twist_joy_node',
+                parameters=[{'config_filepath': params_file_launch_arg,
+                             'publish_stamped_twist': False}],
+                remappings=[
+                    ('/cmd_vel', (robot_name_launch_arg, '/commands/velocity'))]
+            )]
+    )
+
+    return [
+        load_nodes
+    ]
+
+def generate_launch_description():
+    declared_arguments = []
+    declared_arguments.append(DeclareLaunchArgument(
+        'robot_name',
+        default_value='robot',
+        description='Namespace for the robot')
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            'params_file',
+            default_value=PathJoinSubstitution([
+                FindPackageShare('rail_robot'),
+                'config',
+                'joystick_params.yaml'
+            ]),
+            description='Path to the joystick configuration YAML file')
+    )
+
+    return launch.LaunchDescription(declared_arguments + [OpaqueFunction(function=launch_setup)])
