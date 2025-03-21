3fd25333f79a146b243ec04b8940569ef485131e
Abhish Khanal <abheeshkhanal@gmail.com>
2025-03-21 14:54:40 -0400

    update install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ set -e # Exit if a commands exists with a non-zero status
 # Install ros2 packages
 sudo apt update
 sudo apt install -y \
-    openssh-server \
+    ros-dev-tools \
     ros-humble-ros2-control \
     ros-humble-kobuki-ros-interfaces \
     ros-humble-kobuki-velocity-smoother \
@@ -20,7 +20,7 @@ git submodule update --init --recursive
 # Install dependencies
 if [ ! -f /etc/ros/rosdep/sources.list.d/20-default.list ]; then
     echo "rosdep is not initialized. Initializing rosdep..."
-    rosdep init
+    sudo rosdep init
 else
     echo "rosdep is already initialized."
 fi
@@ -34,4 +34,19 @@ sudo service udev restart
 
 # Install
 colcon build --symlink-install
-echo "Installation complete"
+
+if ! grep -Fxq "source /opt/ros/humble/setup.bash" ~/.bashrc; then
+    echo "source /opt/ros/humble/setup.bash" >> ~/.bashrc
+fi
+
+if ! grep -Fxq "source ~/rail_robot/install/setup.bash" ~/.bashrc; then
+    echo "source ~/rail_robot/install/setup.bash" >> ~/.bashrc
+fi
+
+if ! grep -Fxq "export ROS_DOMAIN_ID=42" ~/.bashrc; then
+    echo "export ROS_DOMAIN_ID=42" >> ~/.bashrc
+    echo 'export ROS_IP=$(hostname -I | cut -d " " -f1)' >> ~/.bashrc
+    echo 'export ROS_HOSTNAME=$(hostname -I | cut -d " " -f1)' >> ~/.bashrc
+fi
+
+echo "Installation complete. Use 'source ~/.bashrc' to source the rail_robot package"
