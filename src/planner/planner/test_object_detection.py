fda1f709c3de2b3d8e220a2e26d3748a7d7cd9b7
Abhishek Paudel <10024463+abpaudel@users.noreply.github.com>
2025-07-17 10:31:31 -0400

    add test script for object detection

---
+++ src/planner/planner/test_object_detection.py
@@ -0,0 +1,26 @@
+import os
+from PIL import Image as PILImage
+from . import object_detection
+import rclpy
+
+
+def get_robot_image(robot_name, path='~/mr_task_data'):
+    print(f"Getting image from {robot_name}")
+    path = os.path.expanduser(path)
+    image_path = os.path.join(path, f"{robot_name}_image.png")
+    image = PILImage.open(image_path)
+    return image
+
+
+def main(args=None):
+    # rclpy.init(args=args)
+    robot_name = "robot1"
+    camera_image = get_robot_image(robot_name)
+    objects_to_find = ['pillow', 'remotecontrol']
+    reached_container_name = robot_name
+    objects_found = object_detection.get_revealed_objects(camera_image, objects_to_find, reached_container_name)
+    print(f"Objects found at {reached_container_name}: {objects_found}")
+
+
+if __name__ == '__main__':
+    main()
